e578f0087547d3cc36ad277f3d09bf6739e9c14f
prmc9251 <prmc.9251@gmail.com>
2025-12-05 14:23:14 +0000

    Skeleton of sample-cucumber-project

---
+++ src/test/java/com/nopcommerce/demo/runner/RunCukeTest.java
@@ -0,0 +1,8 @@
+package com.nopcommerce.demo.runner;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class RunCukeTest {
+
+}
---
+++ src/test/java/com/nopcommerce/demo/steps/Hooks.java
@@ -0,0 +1,10 @@
+package com.nopcommerce.demo.steps;
+
+import com.nopcommerce.demo.utility.Utility;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class Hooks extends Utility {
+
+}
